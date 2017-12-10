6558229e7b3c68ce40d060afd20dfd5a98b726e1
abdul-kader138 <babu313136@gmail.com>
2017-12-10 13:15:18 +0600

    Bug Fixing for Inventory Module

--- sma/modules/products/controllers/products.php
+++ sma/modules/products/controllers/products.php
@@ -83,7 +83,7 @@ class Products extends MX_Controller
     function getdatatableajaxcost()
     {
 
-        $userHasAuthority = $this->ion_auth->in_group(array('admin', 'owner'));
+        $userHasAuthority = $this->ion_auth->in_group(array('admin', 'owner','saleman'));
 
         $this->load->library('datatables');
         $this->datatables
