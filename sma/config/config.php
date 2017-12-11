cb9c79092c080dd953f6e5804c4f7989d8bcf0d4
abdul-kader138 <babu313136@gmail.com>
2017-12-11 16:35:06 +0600

    Change DB

--- sma/config/config.php
+++ sma/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/freashmart';
+$config['base_url']	= 'http://localhost/dift_pg/dift_pg/';
 
 /*
 |--------------------------------------------------------------------------
